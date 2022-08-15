4a3a31f601925cfba87e9fe2093473559984d20e
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-15 13:44:03 +0300

    Добавили 4 Основные группы Функций

---
+++ Ex1/Program.cs
@@ -0,0 +1,46 @@
+﻿// 1 Группа методов, коорые ничего не принимают и не возвращают
+void Method1()
+{
+    Console.WriteLine("Автор:ЗДМ");
+}
+//Method1();
+
+
+
+
+//2 Группа методов, коорые ничего не возвращают, но принимают
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2(msg:"текст сообщения");
+
+
+
+
+//3 Группа методов, коорые что-то возвращают, но не принимают
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+//Console.WriteLine(year);
+
+
+
+
+//4 Группа методов, коорые что-то принимают и что-то возвращают
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+    while(i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+string res = Method4(3, "x");
+//Console.WriteLine(res);
+
